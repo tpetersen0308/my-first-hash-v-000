3a95498dc1f82b43c5a47f23e7d4ce6baac42cb8
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 17:56:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {"pi" => "3.1416", "e" => "2.7183", "phi" => "1.618", "tau" => "6.2832"}
   
 end
 
